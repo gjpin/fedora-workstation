102a81c7d228031adeee516f79779a5b1877f61c
Guilherme <3874515+gjpin@users.noreply.github.com>
2022-03-28 22:31:58 +0100

    WIP: update for Fedora 36

--- fedora-gnome.sh
+++ fedora-gnome.sh
@@ -1,16 +1,16 @@
 ##### Versions
-GOLANG_VERSION=1.17.7
+GOLANG_VERSION=1.18
 NOMAD_VERSION=1.2.6
-CONSUL_VERSION=1.11.3
-VAULT_VERSION=1.9.3
-TERRAFORM_VERSION=1.1.6
+CONSUL_VERSION=1.11.4
+VAULT_VERSION=1.10.0
+TERRAFORM_VERSION=1.1.7
+PACKER_VERSION=1.8.0
 
 ##### FOLDERS
 mkdir -p \
 ${HOME}/.bashrc.d/ \
 ${HOME}/.local/bin \
 ${HOME}/.local/share/themes \
-${HOME}/.local/share/icons \
 ${HOME}/src
 
 ##### FLATPAK
@@ -19,21 +19,14 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 flatpak update --appstream
 
-# Allow Flatpaks to access themes and icons
-sudo flatpak override --filesystem=xdg-data/themes:ro
-sudo flatpak override --filesystem=xdg-data/icons:ro
-sudo flatpak override --filesystem=xdg-config/gtk-3.0:ro
-sudo flatpak override --filesystem=xdg-config/gtk-4.0:ro
-
 # Install TOTP and password manager flatpaks
 sudo flatpak install -y flathub org.gnome.World.Secrets
 sudo flatpak install -y flathub com.belmoussaoui.Authenticator
 sudo flatpak override --unshare=network com.belmoussaoui.Authenticator
 
 # Install applications
-sudo flatpak install -y flathub com.spotify.Client
+sudo flatpak install -y flathub dev.alextren.Spot
 sudo flatpak install -y flathub com.usebottles.bottles
-sudo flatpak install -y flathub org.blender.Blender
 sudo flatpak install -y flathub com.github.tchx84.Flatseal
 
 # Install Chrome and enable GPU acceleration
@@ -44,6 +37,8 @@ tee -a ~/.var/app/com.google.Chrome/config/chrome-flags.conf << EOF
 --enable-gpu-rasterization
 --enable-zero-copy
 --enable-features=VaapiVideoDecoder
+--ozone-platform-hint=auto
+--enable-webrtc-pipewire-capturer
 EOF
 
 # Install Chromium and enable GPU acceleration
@@ -54,11 +49,13 @@ tee -a ~/.var/app/org.chromium.Chromium/config/chromium-flags.conf << EOF
 --enable-gpu-rasterization
 --enable-zero-copy
 --enable-features=VaapiVideoDecoder
+--ozone-platform-hint=auto
+--enable-webrtc-pipewire-capturer
 EOF
 
 ##### APPLICATIONS
 # Common software
-sudo dnf install -y ninja-build meson sassc autoconf automake make jq htop
+sudo dnf install -y ninja-build meson sassc autoconf automake make jq htop ffmpeg
 
 # Git
 sudo dnf install -y git-core
@@ -100,8 +97,8 @@ tee -a ${HOME}/.config/Code/User/settings.json << EOF
         "*.hcl": "terraform",
         "*.bu": "yaml",
         "*.ign": "json"
+        "*.service": "ini"
     },
-    "workbench.colorTheme": "GNOME dark",
     "extensions.ignoreRecommendations": true
 }
 EOF
@@ -113,14 +110,13 @@ code --install-extension redhat.ansible
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension editorconfig.editorconfig
 code --install-extension octref.vetur
-code --install-extension rafaelmardojai.vscode-gnome-theme
-code --install-extension fkrull.gtk-dark-titlebar
 
 # Hashistack
 curl -sSL https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/nomad_${NOMAD_VERSION}_linux_amd64.zip -o hashistack-nomad.zip
 curl -sSL https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip -o hashistack-consul.zip
 curl -sSL https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip -o hashistack-vault.zip
 curl -sSL https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip -o hashistack-terraform.zip
+curl -sSL https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip -o hashistack-packer.zip
 unzip 'hashistack-*.zip' -d  ${HOME}/.local/bin
 rm hashistack-*.zip
 
@@ -143,6 +139,7 @@ sudo dnf module install -y nodejs:16/default
 sudo dnf remove -y firefox
 
 # Install Firefox Flatpak
+sudo flatpak install -y flathub org.freedesktop.Platform.ffmpeg-full
 sudo flatpak install -y flathub org.mozilla.firefox
 sudo flatpak override --socket=wayland --env=MOZ_ENABLE_WAYLAND=1 org.mozilla.firefox
 
@@ -150,21 +147,15 @@ sudo flatpak override --socket=wayland --env=MOZ_ENABLE_WAYLAND=1 org.mozilla.fi
 timeout 5 flatpak run org.mozilla.firefox --headless
 
 # Install Firefox theme
-git clone https://github.com/rafaelmardojai/firefox-gnome-theme && cd firefox-gnome-theme
-git checkout libadwaita
-./scripts/install.sh -f ~/.var/app/org.mozilla.firefox/.mozilla/firefox
-cd .. && rm -rf firefox-gnome-theme/
+#git clone https://github.com/rafaelmardojai/firefox-gnome-theme && cd firefox-gnome-theme
+#git checkout libadwaita
+#./scripts/install.sh -f ~/.var/app/org.mozilla.firefox/.mozilla/firefox
+#cd .. && rm -rf firefox-gnome-theme/
 
 # Import Firefox user settings
 cd ${HOME}/.var/app/org.mozilla.firefox/.mozilla/firefox/*-release
 rm user.js
 tee -a user.js << EOF
-// Required by Firefox theme
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("browser.uidensity", 0);
-user_pref("svg.context-properties.content.enabled", true);
-user_pref("ui.useOverlayScrollbars", 1);
-
 // Enable hardware acceleration
 user_pref("media.ffmpeg.vaapi.enabled", true);
 EOF
@@ -174,40 +165,14 @@ cd
 # adw-gtk3 theme (libadwaita ported to GTK3)
 git clone https://github.com/lassekongo83/adw-gtk3.git
 cd adw-gtk3
-meson build
-DESTDIR=${HOME}/.local/share/themes ninja -C build install
-mv ${HOME}/.local/share/themes/usr/share/themes/* ${HOME}/.local/share/themes
-rm -r ${HOME}/.local/share/themes/usr
+meson -Dprefix="${HOME}/.local" build
+ninja -C build install
 cd .. && rm -rf adw-gtk3
 
 gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
 
-# Gnome 42 icons
-git clone --depth=1 https://gitlab.gnome.org/GNOME/adwaita-icon-theme.git
-cd adwaita-icon-theme
-./autogen.sh
-make
-sudo make install
-cd .. && rm -rf adwaita-icon-theme
-
-gsettings set org.gnome.desktop.interface icon-theme Adwaita
-
-# Gnome Terminal
-dconf write /org/gnome/terminal/legacy/theme-variant "'dark'"
-GNOME_TERMINAL_PROFILE=`gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}'`
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ default-size-columns 110
-
-# Noto Fonts
-sudo dnf install -y google-noto-sans-vf-fonts google-noto-serif-vf-fonts google-noto-sans-mono-vf-fonts
-
-# Update font cache
-fc-cache -v
-
-# Set Noto Fonts
-gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 9'
-gsettings set org.gnome.desktop.interface font-name 'Noto Sans 9'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Noto Sans Bold 9'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Noto Sans Mono 10'
+sudo flatpak install -y flathub org.gtk.Gtk3theme.adw-gtk3
+sudo flatpak install -y flathub org.gtk.Gtk3theme.adw-gtk3-dark
 
 ##### UI / UX CONFIGURATIONS
 # Volume
@@ -220,10 +185,6 @@ gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gtk.Settings.FileChooser sort-directories-first true
 # gsettings set org.gnome.nautilus.preferences click-policy 'single'
 gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
-
-# Text editor
-dconf write /org/gnome/gedit/preferences/editor/wrap-mode "'none'"
-dconf write /org/gnome/gedit/preferences/editor/highlight-current-line false
 
 # Laptop specific
 if [[ $(cat /sys/class/dmi/id/chassis_type) -eq 10 ]]
@@ -276,14 +237,4 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Shift><Su
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Shift><Super>dollar']"
 
 ##### GNOME EXTENSIONS
-sudo dnf install -y gnome-extensions-app
-
-# Just Perfection
-wget https://extensions.gnome.org/extension-data/just-perfection-desktopjust-perfection.v18.shell-extension.zip
-gnome-extensions install just-perfection-desktopjust-perfection.v18.shell-extension.zip
-rm just-perfection-desktopjust-perfection.v18.shell-extension.zip
-
-# Blur My Shel
-wget https://extensions.gnome.org/extension-data/blur-my-shellaunetx.v27.shell-extension.zip
-gnome-extensions install blur-my-shellaunetx.v27.shell-extension.zip
-rm blur-my-shellaunetx.v27.shell-extension.zip
+sudo flatpak install -y flathub com.mattjakeman.ExtensionManager
