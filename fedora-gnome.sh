2eb62cedf04ec46c9fe53d3113667b2f3303a0aa
Guilherme <3874515+gjpin@users.noreply.github.com>
2022-04-03 12:09:50 +0100

    Update fedora-gnome.sh

--- fedora-gnome.sh
+++ fedora-gnome.sh
@@ -37,6 +37,7 @@ sudo flatpak install -y flathub com.github.tchx84.Flatseal
 sudo flatpak install -y flathub com.rafaelmardojai.Blanket
 sudo flatpak install -y flathub org.gaphor.Gaphor
 sudo flatpak install -y flathub de.haeckerfelix.Shortwave
+sudo flatpak install -y flathub net.cozic.joplin_desktop
 
 # Install Chrome and enable GPU acceleration
 sudo flatpak install -y flathub-beta com.google.Chrome
