edc4052dd6479f2ce93a7c9ae13832f7a72e41fe
Guilherme <3874515+gjpin@users.noreply.github.com>
2022-04-10 13:06:16 +0100

    enable flathub

--- fedora-gnome.sh
+++ fedora-gnome.sh
@@ -23,6 +23,7 @@ EOF
 # Add Flathub and Flathub Beta repos
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+sudo flatpak remote-modify flathub --enable
 flatpak update --appstream
 
 # Install TOTP and password manager flatpaks
