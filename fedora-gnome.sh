c93e771e29608e0d711e8d19fc184caab6361f9e
Guilherme <3874515+gjpin@users.noreply.github.com>
2022-07-07 10:01:32 +0100

    Update fedora-gnome.sh

--- fedora-gnome.sh
+++ fedora-gnome.sh
@@ -177,6 +177,7 @@ sudo tee /etc/yum.repos.d/tailscale.repo << 'EOF'
 name=Tailscale stable
 baseurl=https://pkgs.tailscale.com/stable/fedora/$basearch
 enabled=1
+type=rpm
 repo_gpgcheck=1
 gpgcheck=0
 gpgkey=https://pkgs.tailscale.com/stable/fedora/repo.gpg
