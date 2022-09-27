2e522d38ef802f4f2553692f795eef87a85d7a45
gjpin <3874515+gjpin@users.noreply.github.com>
2022-09-27 16:35:44 +0100

    Disable turbo boost if on battery

--- fedora-workstation.sh
+++ fedora-workstation.sh
@@ -526,3 +526,67 @@ if cat /proc/cpuinfo | grep "AuthenticAMD" > /dev/null && cat /sys/devices/syste
   sudo grubby --update-kernel=ALL --args="amd_pstate.shared_mem=1"
   echo amd_pstate | sudo tee /etc/modules-load.d/amd-pstate.conf
 fi
+
+################################################
+##### Disable turbo boost if on battery (laptops only)
+################################################
+
+# References:
+# https://chrisdown.name/2017/10/29/adding-power-related-targets-to-systemd.html
+
+# If device is a laptop
+if cat /sys/class/dmi/id/chassis_type | grep 10 > /dev/null; then
+
+# Create systemd AC / battery targets
+sudo tee /etc/systemd/system/ac.target << EOF
+[Unit]
+Description=On AC power
+DefaultDependencies=no
+StopWhenUnneeded=yes
+EOF
+
+sudo tee /etc/systemd/system/battery.target << EOF
+[Unit]
+Description=On battery power
+DefaultDependencies=no
+StopWhenUnneeded=yes
+EOF
+
+# Tell udev to start AC / battery targets when relevant
+sudo tee /etc/udev/rules.d/99-powertargets.rules << 'EOF'
+SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="0", RUN+="/usr/bin/systemctl start battery.target"
+SUBSYSTEM=="power_supply", KERNEL=="AC", ATTR{online}=="1", RUN+="/usr/bin/systemctl start ac.target"
+EOF
+
+# Reload and apply udev's new config
+sudo udevadm control --reload-rules
+
+# Disable turbo boost if on battery 
+sudo tee /etc/systemd/system/disable-turbo-boost.service << EOF
+[Unit]
+Description=Disable turbo boost on battery
+
+[Service]
+Type=oneshot
+ExecStart=-/usr/bin/echo 0 > /sys/devices/system/cpu/cpufreq/boost
+ExecStart=-/usr/bin/echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
+[Install]
+WantedBy=battery.target
+EOF
+
+# Enable turbo boost if on AC 
+sudo tee /etc/systemd/system/enable-turbo-boost.service << EOF
+[Unit]
+Description=Enable turbo boost on AC
+
+[Service]
+Type=oneshot
+ExecStart=-/usr/bin/echo 1 > /sys/devices/system/cpu/cpufreq/boost
+ExecStart=-/usr/bin/echo 0 > /sys/devices/system/cpu/intel_pstate/no_turbo
+
+[Install]
+WantedBy=ac.target
+EOF
+
+fi
